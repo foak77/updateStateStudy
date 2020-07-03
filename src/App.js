a57a68005577637ad8e7f7856d9c7293d3e1a7bd
foak77 <emailtofred@gmail.com>
2020-07-03 11:22:20 -0400

    first Commit stateUpdate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+     meaningOfLife:43
+  }
+
+onHandleClick = () =>{
+  this.setState((prevState, prevProps) => {
+    return { meaningOfLife: prevState.meaningOfLife + 1}
+  },
+    ()=>console.log(this.state.meaningOfLife)
+    )
 }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{this.state.meaningOfLife}</p>
+        <button onClick={this.onHandleClick}>Update State</button>
+      </header>
+    </div>
+    )
+  }
+}
+
+export default App
